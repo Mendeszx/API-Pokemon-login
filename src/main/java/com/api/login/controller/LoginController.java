ac2c094a6b1ee48ece07513daa1258cffb559c98
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2022-09-21 23:19:40 -0300

    update controller

--- src/main/java/com/api/login/controller/LoginController.java
+++ src/main/java/com/api/login/controller/LoginController.java
@@ -1,12 +1,9 @@
 package com.api.login.controller;
 
 import com.api.login.model.LoginModel;
-import com.api.login.model.UserModel;
 import com.api.login.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -21,14 +18,13 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String logar(@RequestBody LoginModel login){
+    public boolean logar(@RequestBody LoginModel login){
         if (repository.existsByEmail(login.getEmail())){
             if (repository.existsBySenha(login.getSenha())){
-                return "https://pokedex-mendes-dev.herokuapp.com/";
+                return true;
             }
-            return "foi email";
         }
-        return "n";
+        return false;
     }
 
 
